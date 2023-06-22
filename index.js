14d5cbc1a39ceec0923452a96ce5a07eac3a55b3
terco89 <valeraquiroz.gb@gmail.com>
2023-06-21 21:52:49 -0300

    siuuuuu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const url = 'https://joyboxapp.000webhost.com/';
-const axios = require('axios');
+var url = 'https://joyboxapp.000webhost.com/';
+const fetch = require('node-fetch');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -23,14 +23,17 @@ io.on('connection',(socket)=>{
       contrasenia:json.contrasenia,
       rcontrasenia:json.rcontrasenia
     };
-    console.log(datos.nombre+" "+datos.correo+" "+datos.edad+" "+datos.contrasenia+" "+datos.rcontrasenia);
-    axios.post("https://joyboxapp.000webhostapp.com/nuevoUsuario.php", datos)
-    .then(response => {
-      io.to(socket.id).emit('registro', JSON.stringify(response));
+    fetch('http://example.com/external-file.php', {
+      method: 'POST',
+      body: postData
     })
-    .catch(error => {
-      io.to(socket.id).emit('registro', JSON.stringify({exito : false}));
-    });
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   });
 });
 
