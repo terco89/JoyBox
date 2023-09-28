f40bb2c94236bb352c46dedeb044a708d9155d0f
terco89 <valeraquiroz.gb@gmail.com>
2023-09-28 07:25:48 -0300

    siuuuu

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const url = 'https://joyboxapp.000webhost.com/';
 const axios = require('axios');
 const CircularJSON = require('circular-json');
+
 
 const { Pool } = require('pg');
 
@@ -53,18 +55,34 @@ const server = app.listen(app.get('port'), () => {
   console.log("servidor se corre en puerto ", app.get('port'));
 });
 
-
 const SocketIO = require('socket.io');
 const io = SocketIO(server);
 
 io.on('connection', (socket) => {
   //console.log('Deconocido con ' + socket.id + ' saluda al server');
-  io.to(socket.id).emit("inicio",[]);
+  io.to(socket.id).emit("inicio", []);
   socket.on("disconnect", (data) => {
-    if(buscarid(socket.id)){
-      for(var i = players.length-1; i >= 0; i--){
-        if(players[i].sid == socket.id){
-          players.splice(i,1)
+    console.log("ds")
+    const id = buscarid(socket.id);
+    enviarConsulta("SELECT id_amigo FROM amigos WHERE fecha_baja IS NULL AND id_user = "+id).then(res => {
+      enviarConsulta("SELECT id_user as id_amigo FROM amigos WHERE fecha_baja IS NULL AND id_amigo = "+id).then(res1 => {
+        var amigos = [...res.rows,...res1.rows]
+        for(var i = 0; i < amigos.length;i++){
+          const sid = buscarsid(amigos[i].id_amigo);
+          if(sid != -1){
+            io.to(sid).emit("enLineaNo",[id])
+          }
+        }
+      }).catch(err1 =>{
+        console.log(err1)
+      })
+    }).catch(err =>{
+      console.log(err)
+    })
+    if (id != -1) {
+      for (var i = players.length - 1; i >= 0; i--) {
+        if (players[i].sid == socket.id) {
+          players.splice(i, 1)
           break;
         }
       }
@@ -73,66 +91,93 @@ io.on('connection', (socket) => {
   socket.on('registro', (data) => {
     data = data.datos;
     console.log(data);
-    enviarConsulta("SELECT registrar('"+data[0]+"',"+data[1]+",'"+data[2]+"','"+data[3]+"','"+data[4]+"') AS msg").then(res=>{
-      io.to(socket.id).emit("registro",{msg:res.rows[0].msg});
+    enviarConsulta("SELECT registrar('" + data[0] + "'," + data[1] + ",'" + data[2] + "','" + data[3] + "','" + data[4] + "') AS msg").then(res => {
+      io.to(socket.id).emit("registro", { msg: res.rows[0].msg });
     })
-    .catch(error => {
-      console.log(error);
-      io.to(socket.id).emit("registro",{msg:"error"});
-    })
+      .catch(error => {
+        console.log(error);
+        io.to(socket.id).emit("registro", { msg: "error" });
+      })
   });
   socket.on('login', (data) => {
     data = data.datos
-    enviarConsulta("SELECT login('"+data[0]+"','"+data[1]+"') AS id").then(res => {
-      if(res.rows[0].id){
-        if(buscarsid(res.rows[0].id)){
-          io.to(socket.id).emit("login", ["","EnUso"]);
+    enviarConsulta("SELECT login('" + data[0] + "','" + data[1] + "') AS id").then(res => {
+      if (res.rows[0].id) {
+        if (buscarsid(res.rows[0].id) != -1) {
+          io.to(socket.id).emit("login", ["", "EnUso"]);
           return;
         }
         enviarConsulta("SELECT id_amigo,nombre FROM amigos INNER JOIN usuarios ON amigos.id_amigo = usuarios.id WHERE amigos.fecha_baja IS NULL AND id_user = " + res.rows[0].id).then(res1 => {
           enviarConsulta("SELECT id_user AS id_amigo,nombre FROM amigos INNER JOIN usuarios ON amigos.id_user = usuarios.id WHERE amigos.fecha_baja IS NULL AND id_amigo = " + res.rows[0].id).then(res2 => {
-            var amigos = [...res1.rows,...res2.rows];
+            var amigos = [...res1.rows, ...res2.rows];
             var ar = [];
             players.push(new player(socket.id, res.rows[0].id))
             for (var i = 0; i < amigos.length; i++) {
               var ban = true;
               ar.push(amigos[i].id_amigo);
               ar.push(amigos[i].nombre);
-              for(var j = 0; j < partidas.length; j++){
-                if(partidas[j].id == amigos[i].id_amigo){
+              for (var j = 0; j < partidas.length; j++) {
+                if (partidas[j].id == amigos[i].id_amigo) {
                   ar.push(partidas[j].juego)
                   ban = false;
                   break;
                 }
               }
-              if(ban){
+              if (ban) {
                 ar.push("defecto")
+              }
+              if(fs.existsSync('img/usuarios/' + amigos[i].id_amigo + '.png')){
+                ar.push(fs.readFileSync('img/usuarios/' + amigos[i].id_amigo + '.png').toString('base64'));
+              }
+              else{
+                ar.push("defecto")
+              }
+              if(buscarsid(amigos[i].id_amigo) == -1){
+                ar.push("n")
+              }
+              else{
+                ar.push("s")
+              }
+              for(var j = 0; j < amigos.length; j++){
+                var si = buscarsid(amigos[j].id_amigo);
+                if(si != -1 && si != amigos[i].id_amigo){
+                  io.to(si).emit("enLinea",[amigos[i].id_amigo.toString()]);
+                }
               }
             }
             io.to(socket.id).emit('camigos', ar);
-            io.to(socket.id).emit('login', [data[0],"1"]);
+            io.to(socket.id).emit('login', [data[0], "1"]);
+            const imagePath = 'img/usuarios/' + res.rows[0].id + '.png';
+            fs.readFile(imagePath, (err, data) => {
+              if (err) {
+                console.error('Error al leer la imagen:', err);
+              } else {
+                const base64Image = data.toString('base64');
+                io.to(socket.id).emit('miImagen', [base64Image.toString()]);
+              }
+            });
           })
         })
-      } else{
-        io.to(socket.id).emit('login', ["","CI"]);
+      } else {
+        io.to(socket.id).emit('login', ["", "CI"]);
       }
     }).catch(error => {
-      io.to(socket.id).emit('login', ["","IX" ]);
+      io.to(socket.id).emit('login', ["", "IX"]);
       console.log(error);
     });
   });
   socket.on('logout', (data) => {
-    for(var i = players.length-1; i >= 0; i--){
-      if(players[i].sid == socket.id){
-        players.splice(i,1)
-        io.to(socket.id).emit("logout",["adios vaquero :,V"]);
+    for (var i = players.length - 1; i >= 0; i--) {
+      if (players[i].sid == socket.id) {
+        players.splice(i, 1)
+        io.to(socket.id).emit("logout", ["adios vaquero :,V"]);
         break;
       }
-    }  
+    }
   });
   socket.on('eliminar', (data) => {
-    enviarConsulta("UPDATE usuarios SET fecha_baja = CURRENT_DATE WHERE id = "+buscarid(socket.id)).then(res => {
-      io.to(socket.id).emit("eliminar",["pipipi"]);
+    enviarConsulta("UPDATE usuarios SET fecha_baja = CURRENT_DATE WHERE id = " + buscarid(socket.id)).then(res => {
+      io.to(socket.id).emit("eliminar", ["pipipi"]);
     }).catch(error => {
       console.log(error);
     })
@@ -140,11 +185,11 @@ io.on('connection', (socket) => {
   socket.on('cambiarContra', (data) => {
     const id = buscarid(socket.id);
     data = data.datos
-    enviarConsulta("SELECT cambiarcontra('"+data[0]+"','"+data[1]+"',"+id+") AS si").then(res => {
-      if(res.rows[0].si){
-        io.to(socket.id).emit("cambiarContra",["1",data[1]]);
-      } else{
-        io.to(socket.id).emit("cambiarContra",["0"]);
+    enviarConsulta("SELECT cambiarcontra('" + data[0] + "','" + data[1] + "'," + id + ") AS si").then(res => {
+      if (res.rows[0].si) {
+        io.to(socket.id).emit("cambiarContra", ["1", data[1]]);
+      } else {
+        io.to(socket.id).emit("cambiarContra", ["0"]);
       }
     }).catch(error => {
       console.log(error);
@@ -153,7 +198,7 @@ io.on('connection', (socket) => {
   socket.on('mensajes', (data) => {
     data = limpiar(data.datos)
     var id = buscarid(socket.id);
-    enviarConsulta("SELECT mensaje,usuario_id FROM mensajes WHERE (usuario_id = "+id+" AND amigo_id = "+data[0]+") OR (usuario_id = "+data[0]+" AND amigo_id = "+id+")").then(res => {
+    enviarConsulta("SELECT mensaje,usuario_id FROM mensajes WHERE (usuario_id = " + id + " AND amigo_id = " + data[0] + ") OR (usuario_id = " + data[0] + " AND amigo_id = " + id + ")").then(res => {
       if (res.rows) {
         var envio = [];
         for (var i = 0; i < res.rows.length; i++) {
@@ -167,18 +212,32 @@ io.on('connection', (socket) => {
     })
   });
   socket.on('mandar', (data) => {
-      data = data.datos
-      var id = buscarid(socket.id)
-      enviarConsulta("INSERT INTO mensajes(usuario_id,amigo_id,mensaje) VALUES("+id+","+data[0]+",'"+data[1]+"')").then(res => {
-          var sid = buscarsid(data[0])
-          if (sid) {
-            io.to(sid).emit("nuevoMensaje", { id: id, msg: data[1] });
-          }
-          io.to(socket.id).emit("mensajeEnviado", { exito: true });
-      }).catch(error => {
-        console.log(error)
-      })
+    data = data.datos
+    var id = buscarid(socket.id)
+    enviarConsulta("INSERT INTO mensajes(usuario_id,amigo_id,mensaje) VALUES(" + id + "," + data[0] + ",'" + data[1] + "')").then(res => {
+      var sid = buscarsid(data[0])
+      if (sid != -1) {
+        io.to(sid).emit("nuevoMensaje", { id: id, msg: data[1] });
+      }
+      io.to(socket.id).emit("mensajeEnviado", { exito: true });
+    }).catch(error => {
+      console.log(error)
+    })
   });
+
+  //imagen de usuario
+
+  socket.on('cargarImagen', (data) => {
+    const imagePath = 'img/usuarios/' + buscarid(socket.id) + '.png';
+    fs.writeFile(imagePath, data.datos, (err) => {
+      if (err)
+        console.log(err);
+      else {
+        io.to(socket.id).emit('imagenCargada', ["si"]);
+      }
+    });
+  });
+  //fin
   socket.on('esteJuego', (data) => {
     var juego = JSON.parse(data).juego;
     var id = buscarid(socket.id);
@@ -204,7 +263,7 @@ io.on('connection', (socket) => {
   socket.on('buscarUsuarios', (data) => {
     data = data.datos
     var id = buscarid(socket.id);
-    enviarConsulta("SELECT id,nombre FROM usuarios WHERE nombre LIKE '%"+data[0]+"%' AND fecha_baja IS NULL AND amigo("+id+",id) = False AND id != "+id).then(res => {
+    enviarConsulta("SELECT id,nombre FROM usuarios WHERE nombre LIKE '%" + data[0] + "%' AND fecha_baja IS NULL AND amigo(" + id + ",id) = False AND id != " + id).then(res => {
       if (res.rows) {
         var envio = [];
         for (var i = 0; i < res.rows.length; i++) {
@@ -213,7 +272,7 @@ io.on('connection', (socket) => {
         }
         io.to(socket.id).emit("losUsuarios", envio);
       }
-      else{
+      else {
         io.to(socket.id).emit("losUsuarios", ["nada"]);
       }
     }).catch(error => {
@@ -223,8 +282,8 @@ io.on('connection', (socket) => {
 
   socket.on('añadirAmigo', (data) => {
     data = data.datos
-    enviarConsulta("INSERT INTO amigos(id_user,id_amigo,fecha_baja) VALUES("+buscarid(socket.id)+","+data[0]+",CURRENT_DATE)").then(res => {
-      io.to(socket.id).emit("añadioAmigo",[data[0]]);
+    enviarConsulta("INSERT INTO amigos(id_user,id_amigo,fecha_baja) VALUES(" + buscarid(socket.id) + "," + data[0] + ",CURRENT_DATE)").then(res => {
+      io.to(socket.id).emit("añadioAmigo", [data[0]]);
     }).catch(error => {
       console.log(error);
     })
@@ -232,7 +291,7 @@ io.on('connection', (socket) => {
 
   socket.on('buscarGrupos', (data) => {
     data = data.datos
-    enviarConsulta("SELECT id,nombre FROM usuarios WHERE nombre LIKE '%"+data[0]+"%'").then(res => {
+    enviarConsulta("SELECT id,nombre FROM usuarios WHERE nombre LIKE '%" + data[0] + "%'").then(res => {
       if (res.rows) {
         var envio = [];
         for (var i = 0; i < res.rows.length; i++) {
@@ -241,7 +300,7 @@ io.on('connection', (socket) => {
         }
         io.to(socket.id).emit("losMensajes", envio);
       }
-      else{
+      else {
         io.to(socket.id).emit("losMensajes", ["nada"]);
       }
     }).catch(error => {
@@ -253,7 +312,7 @@ io.on('connection', (socket) => {
 
   // eventos para las solicitudes de amistad
   socket.on('pedirSolicitudes', (data) => {
-    enviarConsulta("SELECT id_user AS id,nombre FROM amigos INNER JOIN usuarios ON amigos.id_user = usuarios.id WHERE amigos.fecha_baja IS NOT NULL AND id_amigo = "+buscarid(socket.id)).then(res => {
+    enviarConsulta("SELECT id_user AS id,nombre FROM amigos INNER JOIN usuarios ON amigos.id_user = usuarios.id WHERE amigos.fecha_baja IS NOT NULL AND id_amigo = " + buscarid(socket.id)).then(res => {
       if (res.rows) {
         var envio = [];
         for (var i = 0; i < res.rows.length; i++) {
@@ -262,7 +321,7 @@ io.on('connection', (socket) => {
         }
         io.to(socket.id).emit("lasSolicitudes", envio);
       }
-      else{
+      else {
         io.to(socket.id).emit("lasSolicitudes", ["nada"]);
       }
     }).catch(error => {
@@ -270,15 +329,15 @@ io.on('connection', (socket) => {
     })
   });
   socket.on('aceptarSolicitud', (data) => {
-    enviarConsulta("UPDATE amigos SET fecha_baja = null WHERE id_user = "+data.datos[0]+" AND id_amigo = "+buscarid(socket.id)).then(res => {
-        io.to(socket.id).emit("solicitudAceptada", data.datos);
+    enviarConsulta("UPDATE amigos SET fecha_baja = null WHERE id_user = " + data.datos[0] + " AND id_amigo = " + buscarid(socket.id)).then(res => {
+      io.to(socket.id).emit("solicitudAceptada", data.datos);
     }).catch(error => {
       console.log(error);
     })
   });
   socket.on('rechazarSolicitud', (data) => {
-    enviarConsulta("DELETE FROM amigos WHERE id_user = "+data.datos[0]+" AND id_amigo = "+buscarid(socket.id)).then(res => {
-        io.to(socket.id).emit("solicitudRechazada", data.datos);
+    enviarConsulta("DELETE FROM amigos WHERE id_user = " + data.datos[0] + " AND id_amigo = " + buscarid(socket.id)).then(res => {
+      io.to(socket.id).emit("solicitudRechazada", data.datos);
     }).catch(error => {
       console.log(error);
     })
@@ -359,6 +418,7 @@ function buscarid(sid) {
       return players[i].id;
     }
   }
+  return -1;
 }
 
 function buscarsid(id) {
@@ -367,6 +427,7 @@ function buscarsid(id) {
       return players[i].sid;
     }
   }
+  return -1;
 }
 
 function buscarPartida(id) {
@@ -377,25 +438,25 @@ function buscarPartida(id) {
   }
 }
 
-async function enviarConsulta(consulta){
+async function enviarConsulta(consulta) {
   return await pool.query(consulta);
 }
 
-async function pullConsultas(consultas){
+async function pullConsultas(consultas) {
   var respuestas = [];
-  for(var i = 0; i < consultas.length; i++){
-    respuestas[respuestas.length-1] = await enviarConsulta();
+  for (var i = 0; i < consultas.length; i++) {
+    respuestas[respuestas.length - 1] = await enviarConsulta();
   }
   return respuestas;
 }
 
-function limpiar(si){
+function limpiar(si) {
   datos = []
-  for(var i = 0; i < si.length; i++){
-    if(si[i].length > 1){
-      datos.push(si[i].substr(0,si[i].length-1));
+  for (var i = 0; i < si.length; i++) {
+    if (si[i].length > 1) {
+      datos.push(si[i].substr(0, si[i].length - 1));
     }
-    else{
+    else {
       datos.push(si[i])
     }
   }
